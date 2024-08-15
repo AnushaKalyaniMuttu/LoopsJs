3a4f27bc99cab9bcc0d320e51114a1bd6d846c2f
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2024-08-15 11:39:47 +0530

    Create Script2.js

---
+++ Script2.js
@@ -0,0 +1,24 @@
+const jonas=[
+'Jonas',
+'Schmedtmann',
+2037-1991,
+'teacher',
+['Michael','Peter','Steven'],
+true
+];
+
+
+for(let i=0;i<jonas.length;i++){
+console.log(jonas[i], typeof jonas[i]);
+}
+
+
+
+/*
+Jonas string
+Schmedtmann string
+46 number
+teacher string
+[ 'Michael', 'Peter', 'Steven' ] object
+true boolean
+*/
